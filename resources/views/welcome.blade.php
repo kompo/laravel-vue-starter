29d9575ece607909b5be2a9f6a5f7c2ccc0a7812
Bass El Hachem <basselhachem@kompo.io>
2020-09-03 21:53:33 -0400

    Testing that the demo Form works with built assets. All good.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,8 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
         <!-- Styles -->
         <style>
             html, body {
@@ -81,20 +83,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel + Kompo
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div id="app">
+                   {!! App\Http\Komposers\KompoDemoForm::render() !!}
                 </div>
             </div>
         </div>
     </body>
+    <script src="{{ mix('js/app.js') }}"></script>
 </html>
